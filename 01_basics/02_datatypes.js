12ef6338a5c9c3ee36d3a556d56819a0585ed185
ROHIT BISWAS <rohitbiswas030906@gmail.com>
2025-11-15 04:39:28 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all js code as newer version
+
+// alert( 3+ 3) // we are using node js not browser
+
+console.log(3+3)  // code readability should be high
+console.log("Rohit")
+
+
+let name = "vikir"
+let age = 15
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53 ; it's the max range 
+// bigint is for big number
+// strinng => ""
+
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => to find uniqueness
+
+
+// object
+
+console.log(typeof "vikir"); // string
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+
+
